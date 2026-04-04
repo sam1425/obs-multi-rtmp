5e5e7a4ea50ae91cfab0ce57dc14ead2bb35509e
c0mplex <sadasipa@gmail.com>
2026-04-04 09:59:44 -0500

    Fixed installer wrong path

--- .github/scripts/Package-Windows.ps1
+++ .github/scripts/Package-Windows.ps1
@@ -101,7 +101,7 @@ function Package {
     Push-Location -Stack BuildTemp
     Ensure-Location -Path "${ProjectRoot}/release"
     Invoke-External "makensis.exe" "/DCONFIG=${Configuration}" ${NsiFile}
-    Copy-Item -Path "${ProjectRoot}/release/obs-multi-rtmp-setup.exe" -Destination "${OutputName}-Installer.exe"
+    Copy-Item -Path "${ProjectRoot}/obs-multi-rtmp-setup.exe" -Destination "${OutputName}-Installer.exe"
     Pop-Location -Stack BuildTemp
 
     Log-Group
